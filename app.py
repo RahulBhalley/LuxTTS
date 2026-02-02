afdba357130cd3cdf3902b1d49793252e728bf78
Rahul Bhalley <0behinds_secant@icloud.com>
2026-02-02 16:51:16 +0530

    Fixing theme error

--- app.py
+++ app.py
@@ -267,6 +267,5 @@ if __name__ == "__main__":
     demo.launch(
         share=True, 
         server_name="0.0.0.0", 
-        server_port=7860, 
-        theme=gr.themes.Soft()
+        server_port=7860
     )
